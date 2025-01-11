cc526b258cf8da7bc791254613192c84e0b1a1c0
zoe <160427655+lingazoe@users.noreply.github.com>
2025-01-10 20:32:43 -0500

    suspicious change

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector('.base-btn');
 
 button.addEventListener('click', function() {
     
-    document.body.style.backgroundColor = "#FFE8D6"; // New background color
+    document.body.style.backgroundColor = "#2B1E1A"; // New background color
 });
